829f82f9cabf0a5c2ffdd765e53bcff66cbad1fb
Roshan-Kaveri <roshankaveri93@gmail.com>
2024-12-19 23:39:45 +0530

    striver 1

---
+++ src/striver_sde/day1/ClimbingStairs.java
@@ -0,0 +1,53 @@
+package striver_sde.day1;
+import java.io.*;
+        import java.util.*;
+
+class ClimbingStairs {
+    public static void main(String[] args) {
+
+        // taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+
+        // taking total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            // taking count of stairs
+            int m = sc.nextInt();
+
+            // creating an object of class DynamicProgramming
+            Solution_CS obj = new Solution_CS();
+
+            // calling method countWays() of class
+            // DynamicProgramming
+            System.out.println(obj.countWays(m));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+
+class Solution_CS
+{
+    //Function to count number of ways to reach the nth stair.
+    int countWays(int n) {
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+        }
+
+        return dp[n];
+    }
+}
+
