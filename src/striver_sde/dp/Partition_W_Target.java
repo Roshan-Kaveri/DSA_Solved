e08663a9468e385c5853c0ac8247077aadef5f32
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2025-01-03 22:19:32 +0530

    Partition with target

---
+++ src/striver_sde/dp/Partition_W_Target.java
@@ -0,0 +1,72 @@
+package striver_sde.dp;
+
+import java.io.*;
+
+class Partition_W_Target {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+
+        while (tc-- > 0) {
+
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[str.length];
+            for (int i = 0; i < str.length; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+            String[] nk = br.readLine().trim().split(" ");
+            int k = Integer.parseInt(nk[0]);
+            Solution_PWT sln = new Solution_PWT();
+            int ans = sln.countPartitions(a, k);
+
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+
+class Solution_PWT {
+    static int mod =(int)(Math.pow(10,9)+7);
+
+    static int findWays(int[] num, int tar){
+        int n = num.length;
+
+        int prev[] = new int[tar+1];
+
+        if(num[0] == 0) prev[0] = 2;  // 2 cases -pick and not pick
+        else prev[0] = 1;  // 1 case - not pick
+
+        if(num[0]!=0 && num[0]<=tar) prev[num[0]] = 1;  // 1 case -pick
+
+        for(int ind = 1; ind<n; ind++){
+            int cur[]=new int[tar+1];
+            for(int target= 0; target<=tar; target++){
+                int notTaken = prev[target];
+
+                int taken = 0;
+                if(num[ind]<=target)
+                    taken = prev[target-num[ind]];
+
+                cur[target]= (notTaken + taken)%mod;
+            }
+            prev = cur;
+        }
+        return prev[tar];
+    }
+
+    static int countPartitions(int[] arr, int d){
+        int n = arr.length;
+        int totSum = 0;
+        for(int i=0; i<n;i++){
+            totSum += arr[i];
+        }
+
+        //Checking for edge cases
+        if(totSum-d <0 || (totSum-d)%2 ==1) return 0;
+
+        return findWays(arr,(totSum-d)/2);
+    }
+}
