5b599e7c7ad52def23af77fa652569df8612a41f
Roshan-Kaveri <roshankaveri93@gmail.com>
2025-01-07 22:47:10 +0530

    7

---
+++ src/striver_sde/dp/striver/Arrays_Easy.java
@@ -0,0 +1,36 @@
+package striver_sde.dp.striver;
+
+public class Arrays_Easy {
+    class MoveZerosToEND {
+        public void moveZeroes(int[] nums) {
+
+            int j = -1;
+            int n = nums.length;
+
+            for (int i = 0; i < n; i++) {
+                if (nums[i] == 0) {
+                    j = i;
+                    break;
+                }
+            }
+
+            if (j == -1) return;
+
+            for (int i = j + 1; i < n; i++) {
+
+                if (nums[i] != 0) {
+                    // swap
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                    j++;
+                }
+            }
+
+            return;
+
+        }
+    }
+
+
+}
