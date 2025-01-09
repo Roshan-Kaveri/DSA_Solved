d9cd54afbf55f998e3e41f5c28ab3b5184d370cf
Roshan-Kaveri <roshankaveri93@gmail.com>
2025-01-09 23:27:30 +0530

    index with given sum

---
+++ src/gfg_160/arrays/medium/SubArraySumKPosOnly.java
@@ -0,0 +1,31 @@
+package gfg_160.arrays.medium;
+
+import java.util.ArrayList;
+
+public class SubArraySumKPosOnly {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        // code here
+        int n = arr.length;
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(i<n){
+            if(sum == target){
+                ans.add(i+1);
+                ans.add(j);
+                return ans;
+            }
+            else if(sum < target && j<n){
+                sum += arr[j];
+                j++;
+            }
+            else{
+                sum -= arr[i];
+                i++;
+            }
+        }
+        ans.add(-1);
+        return ans;
+    }
+}
