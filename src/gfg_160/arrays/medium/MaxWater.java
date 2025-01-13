b0b8358fc7ab11eb1bbe646fdd6cd1e5eb358f2d
Roshan-Kaveri <roshankaveri93@gmail.com>
2025-01-13 21:28:39 +0530

    max water

---
+++ src/gfg_160/arrays/medium/MaxWater.java
@@ -0,0 +1,27 @@
+package gfg_160.arrays.medium;
+
+public class MaxWater {
+
+
+    public int maxWater(int arr[]) {
+        // Code Here
+        int i=0,j=arr.length-1;
+
+        int max=0;
+
+
+        while(i<j){
+
+            int min=Math.min(arr[i],arr[j]);
+            max = Math.max(max,(min * (j-i)));
+            if(arr[i]<arr[j]){
+                i++;
+            }else{
+                j--;
+            }
+
+        }
+        return max;
+    }
+
+}
