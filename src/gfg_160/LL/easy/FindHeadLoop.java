88c1db39b25649190ffa7c6313226b79f8def9d8
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2025-01-25 20:54:11 +0530

    find head of loop

--- src/gfg_160/LL/easy/FindHeadLoop.java
+++ src/gfg_160/LL/easy/FindHeadLoop.java
@@ -1,2 +1,34 @@
-package gfg_160.LL.easy;public class FindHeadLoop {
+package gfg_160.LL.easy;
+
+public class FindHeadLoop {
+    public static Nodes findFirstNodes(Nodes head) {
+        // code here
+        if(head == null || head.next == null)
+        {
+            return null;
+        }
+        Nodes slow = head;
+        Nodes fast = head;
+
+        while(fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+            if(fast == null)
+            {
+                return null;
+            }
+            if(slow == fast)
+            {
+                Nodes h = head;
+                while(h != fast)
+                {
+                    fast = fast.next;
+                    h = h.next;
+                }
+                return h;
+            }
+        }
+        return null;
+    }
 }
