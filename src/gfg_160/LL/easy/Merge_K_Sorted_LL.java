629884685b970f9995ff224424bb26aab0ecdb34
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2025-02-19 22:40:05 +0530

    merge k sorted LL

--- src/gfg_160/LL/easy/Merge_K_Sorted_LL.java
+++ src/gfg_160/LL/easy/Merge_K_Sorted_LL.java
@@ -1,2 +1,33 @@
-package gfg_160.LL.easy;public class Merge_K_Sorted_LL {
+package gfg_160.LL.easy;
+
+
+
+import gfg_160.trees.Node;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Merge_K_Sorted_LL {
+
+
+        // Function to merge K sorted linked list.
+        Node mergeKLists(List<Node> arr) {
+            // Add your code here.
+            List<Node> list=new ArrayList<>();
+            for(Node node:arr){
+                Node curr=node;
+                while(curr!=null){
+                    list.add(curr);
+                    curr=curr.next;
+                }
+            }
+            Collections.sort(list,(a, b)->a.data-b.data);
+            for(int i=1;i<list.size();i++){
+                list.get(i-1).next=list.get(i);
+            }
+            return list.get(0);
+        }
+
+
 }
--- src/gfg_160/trees/Node.java
+++ src/gfg_160/trees/Node.java
@@ -4,6 +4,7 @@ public class Node {
     public int data;
     public Node right;
     public Node left;
+    public Node next;
 
     public Node(Integer integer) {
     }
