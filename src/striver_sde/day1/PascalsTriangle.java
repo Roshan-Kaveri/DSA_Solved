a31c3ad2b0f1464ece31de3ee9a423367bb9bac8
Roshan-Kaveri <roshankaveri93@gmail.com>
2025-03-21 20:10:35 +0530

    PascalsTriangle

--- src/striver_sde/day1/PascalsTriangle.java
+++ src/striver_sde/day1/PascalsTriangle.java
@@ -1,4 +1,23 @@
 package striver_sde.day1;
 
-public class PascalsTriangle {
+import java.util.ArrayList;
+import java.util.List;
+
+class PascalsTriangle {
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> answer = new ArrayList<>();
+
+        for(int i = 1 ; i <= n ; i++) {
+            int ans = 1;
+            List<Integer> a = new ArrayList<>();
+            a.add(1);
+            for(int j = 1 ; j < i ; j++ ){
+                ans = ans * (i - j) / j;
+                a.add(ans);
+            }
+            answer.add(a);
+        }
+        return answer;
+
+    }
 }
