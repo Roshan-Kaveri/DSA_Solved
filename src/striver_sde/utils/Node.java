8c2bb7f2e338fd98604a84fff8afcf757b128d47
Roshan-Kaveri <161740353+Roshan-Kaveri@users.noreply.github.com>
2025-04-03 23:59:27 +0530

    Clone :LL with random and next

--- src/striver_sde/utils/Node.java
+++ src/striver_sde/utils/Node.java
@@ -3,5 +3,8 @@ package striver_sde.utils;
 public class Node {
     public int data;
     public Node next;
-    Node(int d)  { data = d;  next = null; }
+    public int val;
+    public Node random;
+
+    public Node(int d)  { data = d;  next = null; }
 }
